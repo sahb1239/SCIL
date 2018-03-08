5ac5167ac93c3d7cbc715882308a73372f581843
Søren Bjergmark <sahb1239@gmail.com>
2018-03-08 14:34:38 +0100

    Binary op and unary op

---
+++ SCIL/Instructions/BinaryOp.cs
@@ -0,0 +1,45 @@
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace SCIL.Instructions
+{
+    class BinaryOp : IInstructionEmitter
+    {
+        public string GetCode(TypeDefinition typeDefinition, MethodBody methodBody, Instruction instruction)
+        {
+            // Unsigned and overflow abstracted away
+            switch (instruction.OpCode.Code)
+            {
+                case Code.Add:
+                case Code.Add_Ovf:
+                case Code.Add_Ovf_Un:
+                    return binOp("add");
+                case Code.Sub:
+                case Code.Sub_Ovf:
+                case Code.Sub_Ovf_Un:
+                    return binOp("sub");
+                case Code.Mul:
+                case Code.Mul_Ovf:
+                case Code.Mul_Ovf_Un:
+                    return binOp("mul");
+                case Code.Div:
+                case Code.Div_Un:
+                    return binOp("div");
+                case Code.Rem:
+                case Code.Rem_Un:
+                    return binOp("rem");
+
+                case Code.And:
+                    return binOp("and");
+                case Code.Or:
+                    return binOp("or");
+                case Code.Xor:
+                    return binOp("xor");
+            }
+
+            return null;
+        }
+
+        private string binOp(string op) => op;
+    }
+}
---
+++ SCIL/Instructions/UnaryOp.cs
@@ -0,0 +1,24 @@
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace SCIL.Instructions
+{
+    class UnaryOp : IInstructionEmitter
+    {
+        public string GetCode(TypeDefinition typeDefinition, MethodBody methodBody, Instruction instruction)
+        {
+            // Unsigned and overflow abstracted away
+            switch (instruction.OpCode.Code)
+            {
+                case Code.Neg:
+                    return uOp("neg");
+
+                case Code.Not:
+                    return uOp("not");
+            }
+
+            return null;
+        }
+        private string uOp(string op) => op;
+    }
+}
