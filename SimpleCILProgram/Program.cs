4577bc597217779c0016e8c78e6a010fe5c2ce47
Søren Bjergmark <sahb1239@gmail.com>
2018-04-09 09:46:34 +0200

    Simple CIL program

---
+++ SimpleCILProgram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SimpleCILProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number 1: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int input1))
+            {
+                Console.WriteLine("Not a valid number");
+                return;
+            }
+
+            Console.Write("Input number 2: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int input2))
+            {
+                Console.WriteLine("Not a valid number");
+                return;
+            }
+
+            Console.WriteLine($"{input1} + {input2} = {input1 + input2}");
+            Console.WriteLine($"{input1} - {input2} = {input1 - input2}");
+
+            Console.WriteLine($"{input1} * {input2} = {input1 * input2}");
+            Console.WriteLine($"{input1} / {input2} = {input1 / input2}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
