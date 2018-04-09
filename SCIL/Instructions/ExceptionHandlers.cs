a3ae8070e87345672209e31147ef769fc1d4f4b3
Søren Bjergmark <sahb1239@gmail.com>
2018-04-09 10:19:45 +0200

    Exceptions

---
+++ SCIL/Instructions/ExceptionHandlers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mono.Cecil.Cil;
+
+namespace SCIL.Instructions
+{
+    [EmitterOrder(5)]
+    class ExceptionHandlers : IFlixInstructionGenerator
+    {
+        public string GetCode(MethodBody methodBody, Instruction instruction, IFlixInstructionProgramState programState)
+        {
+            foreach (var exceptionHandler in methodBody.ExceptionHandlers)
+            {
+                if (exceptionHandler.HandlerStart.Offset == instruction.Offset)
+                {
+                    switch (exceptionHandler.HandlerType)
+                    {
+                        case ExceptionHandlerType.Catch:
+                            // Push exception on stack (ugly hack)
+                            programState.PushStack();
+                            //Console.WriteLine();
+
+                            break;
+                        case ExceptionHandlerType.Filter:
+                            break;
+                        case ExceptionHandlerType.Fault:
+                            break;
+                    }
+            }
+        }
+
+            return null;
+        }
+    }
+}
