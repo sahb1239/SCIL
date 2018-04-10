a11241634ff5190a199be7aa6031a78e22543af6
Søren Bjergmark <sahb1239@gmail.com>
2018-04-10 15:18:07 +0200

    Added analyze files

---
+++ AnalyzeSimpleCILProgram.cmd
@@ -0,0 +1,9 @@
+@echo off
+dotnet build SCIL.sln --configuration Release
+
+mkdir Output
+cd Output
+
+dotnet ../SCIL/bin/Release/netcoreapp2.0/SCIL.dll --InputFile "../SimpleCILProgram/bin/Release/netcoreapp2.0/SimpleCILProgram.dll" --OutputPath ./ --countInstructions --singlefile
+
+pause
---
+++ AnalyzeSimpleCILProgram.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+dotnet build SCIL.sln --configuration Release
+
+mkdir Output
+cd Output
+dotnet ../SCIL/bin/Release/netcoreapp2.0/SCIL.dll --InputFile "../SimpleCILProgram/bin/Release/netcoreapp2.0/SimpleCILProgram.dll" --OutputPath ./ --countInstructions --singlefile
