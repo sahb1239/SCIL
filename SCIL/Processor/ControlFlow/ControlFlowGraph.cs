5a2cbd549549ef941aa808c836a69fb707ebde73
Søren Bjergmark <sahb1239@gmail.com>
2018-04-17 10:10:51 +0200

    Moved files

---
+++ SCIL/Processor/ControlFlow/ControlFlowGraph.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace SCIL
+{
+    class ControlFlowGraph
+    {
+        public static Block GenerateBlock(MethodDefinition method)
+        {
+            // Generate blocks
+            var blocks = method.Body.Instructions.Select(instruction => new Block(instruction)).ToList();
+
+            // Set start block
+            Block startBlock = blocks.First();
+
+            // Analyze each block
+            for (var index = 0; index < blocks.Count; index++)
+            {
+                // Get current block
+                var block = blocks[index];
+                
+                // Assert that the block only contains one node
+                Debug.Assert(block.Nodes.Count() == 1);
+
+                // Get node
+                var node = block.Nodes.First();
+
+                switch (node.Instruction.OpCode.FlowControl)
+                {
+                    case FlowControl.Call:
+                    case FlowControl.Next:
+                        block.AddTarget(blocks[index + 1]);
+                        break;
+                    case FlowControl.Branch:
+                        Instruction branchToInstruction = (Instruction) node.Instruction.Operand;
+                        Block branchTargetBlock =
+                            blocks.Single(bl => bl.Nodes.Any(e => e.Instruction == branchToInstruction));
+
+                        block.AddTarget(branchTargetBlock);
+                        break;
+                    case FlowControl.Cond_Branch:
+                        // Add next
+                        block.AddTarget(blocks[index + 1]);
+
+                        // Add branch target
+                        Instruction condBranchToInstruction = (Instruction)node.Instruction.Operand;
+                        Block condBranchTargetBlock =
+                            blocks.Single(bl => bl.Nodes.Any(e => e.Instruction == condBranchToInstruction));
+
+                        block.AddTarget(condBranchTargetBlock);
+                        break;
+                    case FlowControl.Return:
+                        break;
+                    case FlowControl.Throw:
+                        break;
+                    default:
+                        Console.WriteLine("Not handled!");
+                        break;
+                }
+            }
+
+            // Analyze the exception handlers and add as target for each
+            foreach (var handler in method.Body.ExceptionHandlers)
+            {
+                // TODO: Filter is not avalible in C# and is therefore not handled here
+                // Get target block
+                Instruction catchInstructionStart = handler.HandlerStart;
+                Block catchInstructionBlock =
+                    blocks.Single(bl => bl.Nodes.Any(e => e.Instruction == catchInstructionStart));
+
+                Instruction currentInstruction = handler.TryStart;
+                do
+                {
+                    // Get currentInstruction block
+                    var currentInstructionBlock =
+                        blocks.Single(bl => bl.Nodes.Any(e => e.Instruction == currentInstruction));
+
+                    // Add target
+                    currentInstructionBlock.AddTarget(catchInstructionBlock);
+
+                    // Set next instruction
+                    if (currentInstruction.Next == null) break;
+                    currentInstruction = currentInstruction.Next;
+                } while (currentInstruction.Previous == null || currentInstruction != handler.TryEnd);
+            }
+
+            // Remove all which cannot be targeted (remove dead code)
+            // Only very simple dead code is removed
+            // Skip first since it does not have any sources (maybe)
+            List<Block> removeBlocks = new List<Block>();
+            foreach (var block in blocks.Skip(1))
+            {
+                // Test if the block is not targeted by anyone
+                if (!block.Sources.Any())
+                {
+                    removeBlocks.Add(block);
+                }
+            }
+
+            // Remove the blocks
+            removeBlocks.ForEach(block => blocks.Remove(block));
+
+            // Optimize graph
+            for (var index = 0; index < blocks.Count; index++)
+            {
+                var block = blocks[index];
+
+                // While can concat with the next block
+                while (index + 1 < blocks.Count && block.CanConcat(blocks[index + 1]))
+                {
+                    // Concat the block
+                    block.ConcatBlock(blocks[index + 1]);
+
+                    // Remove the block
+                    blocks.RemoveAt(index + 1);
+                }
+            }
+
+            return startBlock;
+        }
+
+        
+    }
+}
---
+++ SCIL/Processor/ControlFlow/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Mono.Cecil.Cil;
+
+namespace SCIL
+{
+    public class Node
+    {
+        public Node(Instruction instruction, Block block)
+        {
+            Instruction = instruction ?? throw new ArgumentNullException(nameof(instruction));
+            Block = block ?? throw new ArgumentNullException(nameof(block));
+        }
+
+        public Instruction Instruction { get; }
+        public Block Block { get; set; }
+
+        public override string ToString()
+        {
+            return Instruction.ToString();
+        }
+    }
+}
