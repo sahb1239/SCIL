b9e42a79e80e263d4ce3005e97dca150c6ca0ce9
Søren Bjergmark <sahb1239@gmail.com>
2018-04-30 11:13:28 +0200

    Fixes

--- SCIL/Processor/Nodes/Node.cs
+++ SCIL/Processor/Nodes/Node.cs
@@ -34,115 +34,144 @@ namespace SCIL
             this.Block.ReplaceNode(this, nodes);
         }
 
+        public int PopCountFromStack
+        {
+            get
+            {
+                int pop;
+                switch (OpCode.StackBehaviourPop)
+                {
+                    case StackBehaviour.Pop0:
+                        pop = 0;
+                        break;
+                    case StackBehaviour.Pop1:
+                    case StackBehaviour.Popi:
+                        pop = 1;
+                        break;
+                    case StackBehaviour.Popref:
+                        pop = 1;
+                        break;
+                    case StackBehaviour.Pop1_pop1:
+                    case StackBehaviour.Popi_pop1:
+                    case StackBehaviour.Popi_popi:
+                    case StackBehaviour.Popi_popr4:
+                    case StackBehaviour.Popi_popr8:
+                        pop = 2;
+                        break;
+                    case StackBehaviour.Popref_popi_popi:
+                    case StackBehaviour.Popref_popi_popr4:
+                    case StackBehaviour.Popref_popi_popr8:
+                    case StackBehaviour.Popref_popi_popi8:
+                    case StackBehaviour.Popref_popi_popref:
+                        pop = 3;
+                        break;
+                    case StackBehaviour.Varpop: // Pop from variables
+                        pop = 0;
+                        break;
+                    case StackBehaviour.PopAll:
+                        pop = 0;
+                        break;
+                    default:
+                        throw new NotImplementedException($"StackBehaviour on pop {OpCode.StackBehaviourPop} not implemented");
+                }
+                
+                // Detect method calling
+                switch (OpCode.Code)
+                {
+                    case Code.Call:
+                    case Code.Calli:
+                    case Code.Callvirt:
+                        // Detect the required numbers of parameters to pop
+                        var method = (MethodReference)Operand;
+                        var parameters = method.Parameters.Count;
+
+                        // Detect this
+                        if (method.HasThis)
+                        {
+                            parameters++;
+                        }
+
+                        pop = parameters;
+
+                        break;
+                }
+
+                // Detect return
+                if (OpCode.Code == Code.Ret && this.Block.Method.Definition.ReturnType.FullName != "System.Void")
+                {
+                    pop = 1;
+                }
+
+                return pop;
+            }
+        }
+
+        public int PushCountFromStack
+        {
+            get
+            {
+                int push;
+                switch (OpCode.StackBehaviourPush)
+                {
+                    case StackBehaviour.Push0:
+                        push = 0;
+                        break;
+                    case StackBehaviour.Push1:
+                    case StackBehaviour.Pushi:
+                    case StackBehaviour.Pushi8:
+                    case StackBehaviour.Pushr4:
+                    case StackBehaviour.Pushr8:
+                    case StackBehaviour.Pushref:
+                        push = 1;
+                        break;
+                    case StackBehaviour.Push1_push1:
+                        push = 2;
+                        break;
+                    case StackBehaviour.Varpush:
+                        // Push to variable
+                        push = 0;
+                        break;
+                    default:
+                        throw new NotImplementedException($"StackBehaviour on push {OpCode.StackBehaviourPush} not implemented");
+                }
+
+
+                // Detect method calling
+                switch (OpCode.Code)
+                {
+                    case Code.Call:
+                    case Code.Calli:
+                    case Code.Callvirt:
+                        // Get method
+                        var method = (MethodReference)Operand;
+
+                        // Detect the required arguments to push
+                        if (method.ReturnType.FullName == "System.Void")
+                        {
+                            push = 0;
+                        }
+                        else
+                        {
+                            push = 1;
+                        }
+
+                        break;
+                }
+
+                // Detect exception handling
+                if (this.Block.Method.Definition.Body.ExceptionHandlers.Any(e => e.HandlerStart == Instruction))
+                {
+                    // We recieve the exception in start of exception
+                    push += 1;
+                }
+
+                return push;
+            }
+        }
+
         public (int popNames, int pushNames) GetRequiredNames()
         {
-            int pop;
-            switch (OpCode.StackBehaviourPop)
-            {
-                case StackBehaviour.Pop0:
-                    pop = 0;
-                    break;
-                case StackBehaviour.Pop1:
-                case StackBehaviour.Popi:
-                    pop = 1;
-                    break;
-                case StackBehaviour.Popref:
-                    pop = 1;
-                    break;
-                case StackBehaviour.Pop1_pop1:
-                case StackBehaviour.Popi_pop1:
-                case StackBehaviour.Popi_popi:
-                case StackBehaviour.Popi_popr4:
-                case StackBehaviour.Popi_popr8:
-                    pop = 2;
-                    break;
-                case StackBehaviour.Popref_popi_popi:
-                case StackBehaviour.Popref_popi_popr4:
-                case StackBehaviour.Popref_popi_popr8:
-                case StackBehaviour.Popref_popi_popi8:
-                case StackBehaviour.Popref_popi_popref:
-                    pop = 3;
-                    break;
-                case StackBehaviour.Varpop: // Pop from variables
-                    pop = 0;
-                    break;
-                case StackBehaviour.PopAll:
-                    pop = 0;
-                    break;
-                default:
-                    throw new NotImplementedException($"StackBehaviour on pop {OpCode.StackBehaviourPop} not implemented");
-            }
-
-            int push;
-            switch (OpCode.StackBehaviourPush)
-            {
-                case StackBehaviour.Push0:
-                    push = 0;
-                    break;
-                case StackBehaviour.Push1:
-                case StackBehaviour.Pushi:
-                case StackBehaviour.Pushi8:
-                case StackBehaviour.Pushr4:
-                case StackBehaviour.Pushr8:
-                case StackBehaviour.Pushref:
-                    push = 1;
-                    break;
-                case StackBehaviour.Push1_push1:
-                    push = 2;
-                    break;
-                case StackBehaviour.Varpush: 
-                    // Push to variable
-                    push = 0;
-                    break;
-                default:
-                    throw new NotImplementedException($"StackBehaviour on push {OpCode.StackBehaviourPush} not implemented");
-            }
-
-            // Detect method calling
-            switch (OpCode.Code)
-            {
-                case Code.Call:
-                case Code.Calli:
-                case Code.Callvirt:
-                    // Detect the required numbers of parameters to pop
-                    var method = (MethodReference) Operand;
-                    var parameters = method.Parameters.Count;
-
-                    // Detect this
-                    if (method.HasThis)
-                    {
-                        parameters++;
-                    }
-
-                    pop = parameters;
-
-                    // Detect the required arguments to push
-                    if (method.ReturnType.FullName == "System.Void")
-                    {
-                        push = 0;
-                    }
-                    else
-                    {
-                        push = 1;
-                    }
-
-                    break;
-            }
-
-            // Detect return
-            if (OpCode.Code == Code.Ret && this.Block.Method.Definition.ReturnType.FullName != "System.Void")
-            {
-                pop = 1;
-            }
-
-            // Detect exception handling
-            if (this.Block.Method.Definition.Body.ExceptionHandlers.Any(e => e.HandlerStart == Instruction))
-            {
-                // We recieve the exception in start of exception
-                push += 1;
-            }
-
-            return (pop, push);
+            return (PopCountFromStack, PushCountFromStack);
         }
 
         private int GetVariableIndex(object operand)
