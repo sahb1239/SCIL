f289ccb8283b909ef6ce93bcd0819e1d8527bf62
Søren Bjergmark <sahb1239@gmail.com>
2018-05-07 12:23:23 +0200

    Fixes to switch rewriter

--- SCIL/Processor/Simplifiers/SwitchRewriterVisitor.cs
+++ SCIL/Processor/Simplifiers/SwitchRewriterVisitor.cs
@@ -25,20 +25,17 @@ namespace SCIL.Processor.Simplifiers
                     targetBlock.Nodes.FirstOrDefault()?.Instruction == targetInstruction)).ToList();
 
                 // Get next target
-                List<Block> otherTargets = node.Block.Targets.Except(blockTargets).ToList();
+                List<Block> otherTargets = node.Block.Targets.ToList();
+                foreach (var target in new List<Block>(blockTargets))
+                {
+                    // Only removes the first occurence
+                    otherTargets.Remove(target);
+                }
 
                 // Get next target
-                Block nextTarget;
-                if (otherTargets.Count == 1)
-                {
-                    nextTarget = otherTargets.First();
-                }
-                else
-                {
-                    var lastNodeInCurrentBlock = node.Block.Nodes.Last();
-                    nextTarget = otherTargets.Single(e =>
-                        e.Nodes.Any(n => n.Instruction == lastNodeInCurrentBlock.Instruction.Next));
-                }
+                var lastNodeInCurrentBlock = node.Block.Nodes.Last();
+                Block nextTarget = node.Block.Targets.First(e =>
+                    e.Nodes.Any(n => n.Instruction == lastNodeInCurrentBlock.Instruction.Next));
 
                 // Update other targets
                 otherTargets = otherTargets.Except(new[] {nextTarget}).ToList();
@@ -46,10 +43,12 @@ namespace SCIL.Processor.Simplifiers
                 // Theese other targets should all be exception handlers if not - we have done something wrong
                 Debug.Assert(otherTargets.All(target => target.Method.Definition.Body.ExceptionHandlers.Any(handler => handler.HandlerStart == target.Nodes.First().Instruction)));
                 
-                // Add a list of new blocks
-                var newBlocks = new List<Block>();
+                // Currently we have a control flow of the following
+                // node.Sources -> node.block -> node.Targets (blockTargets, otherTargets, nexttarget)
+                // We now want to update node.block to use branching
 
-                // TODO: We need to split the blocks
+                // Add a list of new blocks
+                var branchingBlocks = new List<Block>();
                 // Start creating new instructions
                 // Switch is always 0 indexed (i think)
                 // Lets use dup and pop (we could have used variables, however which variable can we just use?)
@@ -61,7 +60,7 @@ namespace SCIL.Processor.Simplifiers
                     var newNodes = new List<Node>();
 
                     // Add dup stm such that we can use it for next instruction
-                    newNodes.Add(new Node(node.Instruction, node.Block) { OverrideOpCode = OpCodes.Dup });
+                    newNodes.Add(new Node(node.Instruction, node.Block) {OverrideOpCode = OpCodes.Dup});
 
                     // Load compare to onto stack
                     newNodes.Add(new Node(node.Instruction, node.Block) { OverrideOpCode = OpCodes.Ldc_I4, OverrideOperand = i });
@@ -74,39 +73,42 @@ namespace SCIL.Processor.Simplifiers
                     newNodes.Add(new Node(node.Instruction, node.Block) { OverrideOpCode = OpCodes.Brtrue, OverrideOperand = target.Nodes.First().Instruction });
 
                     // Add the nodes to a new block
-                    var newBlock = new Block(newNodes.ToArray()) {Method = node.Block.Method};
+                    var newBlock = new Block(newNodes.ToArray()) { Method = node.Block.Method };
 
                     // Update the block
                     newBlock.Nodes.ForEach(n => n.Block = newBlock);
 
-                    // Add target to the new block
+                    // Add target from new block to pop block
                     newBlock.AddTarget(target);
 
                     // Remove target from old block
                     node.Block.RemoveTarget(target);
 
-                    // Add to new blocks
-                    newBlocks.Add(newBlock);
+                    // Add to the list of blocks
+                    branchingBlocks.Add(newBlock);
                 }
 
                 // Add targets to next for all new blocks
                 // Minus 1 since last should target next block and we don't want a IndexOutOfRangeException
-                for (int i = 0; i < newBlocks.Count - 1; i++)
+                for (int i = 0; i < branchingBlocks.Count - 1; i++)
                 {
-                    newBlocks[i].AddTarget(newBlocks[i + 1]);
+                    branchingBlocks[i].AddTarget(branchingBlocks[i + 1]);
                 }
 
                 // Add target to first block
-                node.Block.AddTarget(newBlocks.First());
+                node.Block.AddTarget(branchingBlocks.First());
 
                 // Add next target for last block
-                newBlocks.Last().AddTarget(nextTarget);
+                branchingBlocks.Last().AddTarget(nextTarget);
 
                 // Remove next from current block
                 node.Block.RemoveTarget(nextTarget);
 
-                // Add other targets to all blocks
-                foreach (var block in newBlocks)
+                // We now have
+                // node.Sources -> node.block -> branchingBlock1 -> branchingBlock2 -> node.Targets (blockTargets, otherTargets, nexttarget)
+
+                // Add exception targets to all blocks
+                foreach (var block in branchingBlocks)
                 {
                     foreach (var otherTarget in otherTargets)
                     {
@@ -114,58 +116,107 @@ namespace SCIL.Processor.Simplifiers
                     }
                 }
 
-                // Add pop instruction to each target
-                foreach (var target in blockTargets.Distinct())
-                {
-                    // Add pop instruction at target to move duplicated variable on stack we inserted using dup
-                    target.InsertNodesAtIndex(0,
-                        new Node(target.Nodes.First().Instruction, target) { OverrideOpCode = OpCodes.Pop });
-                }
-
-                // Add pop to next block (to remove dup from all the branching)
-                nextTarget.InsertNodesAtIndex(0,
-                    new Node(nextTarget.Nodes.First().Instruction, nextTarget) {OverrideOpCode = OpCodes.Pop});
-                
                 // Add the new blocks to the method
                 var indexOfSwitch = node.Block.Method.Blocks.ToList().IndexOf(node.Block);
 
                 // We need to insert just after the switch
-                node.Block.Method.Insert(indexOfSwitch + 1, newBlocks.ToArray());
+                node.Block.Method.Insert(indexOfSwitch + 1, branchingBlocks.ToArray());
 
-                // Remove the switch from the current block
-                node.Block.Remove(node);
+                // We now insert pop to next block (after switch)
+                InsertPopBeforeTarget(nextTarget, branchingBlocks, otherTargets);
 
-                // If the block is now empty we need to remove it
-                if (!node.Block.Nodes.Any())
-                {
-                    node.Block.Method.Remove(node.Block);
+                // We now needs to insert pop stm to remove all the extra values
+                InsertPopToAllTargets(blockTargets, branchingBlocks, otherTargets);
 
-                    // Fix sources and add target to new block
-                    foreach (var source in node.Block.Sources.ToList())
-                    {
-                        // source -> node.Block -> newBlocks.First()
-                        // Remove the node from source (first arrow)
-                        source.RemoveTarget(node.Block);
-
-                        // Remove target to the first new block (second arrow)
-                        node.Block.RemoveTarget(newBlocks.First());
-
-                        // Add target from the old source to new new first block (add connection between source and newBlocks.First())
-                        source.AddTarget(newBlocks.First());
-                    }
-
-                    // Remove exception handlers
-                    foreach (var other in otherTargets)
-                    {
-                        node.Block.RemoveTarget(other);
-                    }
-
-                    Debug.Assert(!node.Block.Targets.Any());
-                    Debug.Assert(!node.Block.Sources.Any());
-                }
+                // Remove the switch
+                RemoveSwitchFromCurrentBlock(node, branchingBlocks, otherTargets);
             }
 
             base.Visit(node);
+        }
+
+        private void InsertPopBeforeTarget(Block target, List<Block> branchingBlocks, List<Block> exceptionTargets)
+        {
+            var otherSources = target.Sources.Except(branchingBlocks);
+            if (!otherSources.Any())
+            {
+                // We can safely just insert a pop instruction into the nodes
+                target.InsertNodesAtIndex(0,
+                    new Node(target.Nodes.First().Instruction, target) { OverrideOpCode = OpCodes.Pop });
+            }
+            else
+            {
+                // We need to insert a new block... :(
+                Block newBlock =
+                    new Block(new Node(target.Nodes.First().Instruction, target) { OverrideOpCode = OpCodes.Pop }) { Method = target.Method };
+                newBlock.Nodes.ForEach(n => n.Block = newBlock);
+
+                // We now add the sources (and removes them)
+                var sourcesToAdd = target.Sources.Except(otherSources).ToList();
+                foreach (var source in sourcesToAdd)
+                {
+                    source.AddTarget(newBlock);
+                    source.RemoveTarget(target);
+                }
+
+                // Add target to the new block
+                newBlock.AddTarget(target);
+
+                // We now add the exception targets
+                foreach (var exceptionTarget in exceptionTargets)
+                {
+                    newBlock.AddTarget(exceptionTarget);
+                }
+
+                // Add the new blocks to the method
+                var indexOfTarget = target.Method.Blocks.ToList().IndexOf(target);
+
+                // We need to insert just before the target
+                target.Method.Insert(indexOfTarget, newBlock);
+            }
+        }
+
+        private void InsertPopToAllTargets(List<Block> blockTargets, List<Block> branchingBlocks, List<Block> exceptionTargets)
+        {
+            foreach (var target in blockTargets.Distinct())
+            {
+                InsertPopBeforeTarget(target, branchingBlocks, exceptionTargets);
+            }
+        }
+
+        private void RemoveSwitchFromCurrentBlock(Node node, List<Block> newBlocks, List<Block> exceptionTargets)
+        {
+            // Remove the switch from the current block
+            node.Block.Remove(node);
+
+            // If the block is now empty we need to remove it
+            if (!node.Block.Nodes.Any())
+            {
+                node.Block.Method.Remove(node.Block);
+
+                // Fix sources and add target to new block
+                foreach (var source in node.Block.Sources.ToList())
+                {
+                    // source -> node.Block -> newBlocks.First()
+                    // Remove the node from source (first arrow)
+                    source.RemoveTarget(node.Block);
+
+                    // Remove target to the first new block (second arrow)
+                    node.Block.RemoveTarget(newBlocks.First());
+
+                    // Add target from the old source to new new first block (add connection between source and newBlocks.First())
+                    source.AddTarget(newBlocks.First());
+                }
+
+                // Remove exception handlers
+                foreach (var other in exceptionTargets)
+                {
+                    node.Block.RemoveTarget(other);
+                }
+
+                Debug.Assert(!node.Block.Targets.Any());
+                Debug.Assert(!node.Block.Sources.Any());
+            }
         }
     }
 }
