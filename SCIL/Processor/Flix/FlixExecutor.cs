1f2c31a1fdb290e74cf21d306d1fe8319fda45e3
Mikkel Christensen <mikkel@mclc.dk>
2018-05-07 09:27:04 +0200

    Added flix parameter

--- SCIL/Processor/Flix/FlixExecutor.cs
+++ SCIL/Processor/Flix/FlixExecutor.cs
@@ -126,7 +126,7 @@ namespace SCIL.Flix
             {
                 arguments = arguments.Concat(new List<string>()
                 {
-                    "--print Sources,Sinks,TaintListStack,TaintListLocalVar,TaintListArg,Results"
+                    "--print Sources,Sinks,TaintListStack,TaintListLocalVar,TaintListArg,PointerTable,Results"
                 });
             }
 
