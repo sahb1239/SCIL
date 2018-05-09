235139a9fabe51fbac09e819afa1b03e4f0e85da
Søren Bjergmark <sahb1239@gmail.com>
2018-05-09 11:11:05 +0200

    Fixed edge case in System.dll

--- SCIL/Processor/ControlFlow/SSA/Helpers/SharedNames.cs
+++ SCIL/Processor/ControlFlow/SSA/Helpers/SharedNames.cs
@@ -33,7 +33,29 @@ namespace SCIL.Processor.ControlFlow.SSA.Helpers
 
             // Get index list
             var indexList = _names[index];
-            return indexList.Last();
+
+            // Can be not assigned yet (for example if stack has not been used yet)
+            // For example System.Net.TimerThread.OnDomainUnload
+            /*
+             *
+                .try
+                {
+                  // [75 9 - 75 38]
+                  IL_0000: call         void System.Net.TimerThread::StopTimerThread()
+
+                  IL_0005: leave.s      IL_000a
+                } // end of .try
+                catch [mscorlib]System.Object
+                {
+
+                  // [77 7 - 77 12]
+                  IL_0007: pop          
+
+                  IL_0008: leave.s      IL_000a
+                } // end of catch
+             */
+            // IL_0007 is not assigned to any parent variable and therefore will not be created
+            return indexList.LastOrDefault() ?? "NIL";
         }
     }
 }
