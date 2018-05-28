30bd4eb47d9bc23c620d58e7f6c1860d83aa62ae
Søren Bjergmark <sahb1239@gmail.com>
2018-05-28 10:51:29 +0200

    Added support for additional information on Elements

--- SCIL/Processor/Nodes/Element.cs
+++ SCIL/Processor/Nodes/Element.cs
@@ -1,9 +1,25 @@
-﻿using SCIL.Processor.Nodes.Visitor;
+﻿using System.Collections.Generic;
+using System.Linq;
+using SCIL.Processor.Nodes.Visitor;
 
 namespace SCIL.Processor.Nodes
 {
     public abstract class Element
     {
         public abstract void Accept(IVisitor visitor);
+
+        private readonly IList<object> _information = new List<object>();
+
+        public void AddElement<TElement>(TElement element)
+            where TElement : class
+        {
+            _information.Add(element);
+        }
+
+        public IEnumerable<TElement> GetElementInformations<TElement>()
+            where TElement : class
+        {
+            return _information.OfType<TElement>();
+        }
     }
 }
