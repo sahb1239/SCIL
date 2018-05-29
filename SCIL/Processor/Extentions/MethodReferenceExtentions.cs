be8114a71d7129d4bc2c10c2361348ba54fa852b
Søren Bjergmark <sahb1239@gmail.com>
2018-05-29 13:54:23 +0200

    Moved NameOnly extention method

--- SCIL/Processor/Extentions/MethodReferenceExtentions.cs
+++ SCIL/Processor/Extentions/MethodReferenceExtentions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Mono.Cecil;
 
@@ -7,6 +8,14 @@ namespace SCIL.Processor.Extentions
 {
     public static class MethodReferenceExtentions
     {
+        public static string NameOnly(this MethodReference methodReference)
+        {
+            if (methodReference == null) throw new ArgumentNullException(nameof(methodReference));
+
+            var splitted = methodReference.FullName.Split(' ');
+            return splitted[splitted.Length - 1];
+        }
+
         public static bool IsVoid(this MethodReference methodReference)
         {
             if (methodReference == null) throw new ArgumentNullException(nameof(methodReference));
