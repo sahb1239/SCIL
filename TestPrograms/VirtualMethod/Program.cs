42791720b67ad3200c60288e349a12950263246b
Søren Bjergmark <sahb1239@gmail.com>
2018-05-30 12:06:53 +0200

    Added test case for virtual methods

---
+++ TestPrograms/VirtualMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VirtualMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            GetA().Test(input);
+        }
+
+        static A GetA()
+        {
+            return new B();
+        }
+    }
+
+    class A
+    {
+        public virtual string Test(string str)
+        {
+            return str;
+        }
+    }
+
+    class B : A
+    {
+        public override string Test(string str)
+        {
+            Console.WriteLine(str);
+            return base.Test(str);
+        }
+    }
+}
